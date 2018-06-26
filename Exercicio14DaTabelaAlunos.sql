9cdcaa9e5b24960c4dd46b8eb746f335f69d4d40
AdrianBSilva <38699788+AdrianBSilva@users.noreply.github.com>
2018-06-26 14:40:50 -0300

    .

---
+++ Exercicio14DaTabelaAlunos.sql
@@ -0,0 +1,5 @@
+﻿SELECT nome,CAST((nota_1 + nota_2 + nota_3 + nota_4)/4 as numeric(15,2)) 'Média',
+IIF((nota_1 + nota_2 + nota_3 + nota_4)/4 < 5, 'Reprovado',
+IIF((nota_1 + nota_2 + nota_3 + nota_4)/4 < 7, 'Recuperação', 'Aprovado'))'SITUAÇÃO' FROM alunos
+
+
